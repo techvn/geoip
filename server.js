63238e42388ce08eb22b4453a09dd5db078cb4a9
binhnt <techvn2012@gmail.com>
2014-11-27 15:01:16 -0800

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.logger('dev'));
 
 var geoip = require('geoip-lite');
 
-app.get('/api', function(req, res){
+/*app.get('/api', function(req, res){
 	var ip = req.connection.remoteAddress;
     //console.log(ip);
 	//var ip = "207.97.227.239";
@@ -20,16 +20,26 @@ app.get('/api', function(req, res){
 	
 	// l(toSendBack);
 	res.json(toSendBack);
-});
+});*/
+
 app.get('/api/mylocation', function(req, res){
     var ip = req.connection.remoteAddress;
-    var geo = geoip.lookup(ip);
-    if (geo)
-        var toSendBack = [ip, geo.country, geo.region, geo.city];//.join(",");
-    else
-        var toSendBack = "Unknown";
+
+    var not_allow = ['117.0.33.158'];
+    var data = [];
+
+    if(not_allow.indexOf(ip) == -1) {
+        var geo = geoip.lookup(ip);
+        if (geo)
+            data = [ip, geo.country, geo.region, geo.city];//.join(",");
+        else
+            data = ['Unknown'];
+    } else {
+        data = ['Not allow'];
+    }
+
     // l(toSendBack);
-    res.json(toSendBack);
+    res.json(data);
 });
 
 l("Listening to 8080 ...");
